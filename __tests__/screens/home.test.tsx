1cf87bdec68c2684789e2a968743895037efc221
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-10-04 16:20:12 +0800

    test: add more snapshots

---
+++ __tests__/screens/home.test.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Home } from '../../src/screens/home'
+import { render } from '@testing-library/react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+// fix react-navigation issues:
+// - https://github.com/react-navigation/react-navigation/issues/9727
+// - https://github.com/react-navigation/react-navigation/issues/9568
+jest.useFakeTimers()
+
+const Stack = createNativeStackNavigator()
+
+it('renders correctly', () => {
+  const component = (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Screen' component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+
+  const { toJSON } = render(component)
+
+  expect(toJSON()).toMatchSnapshot()
+})
---
+++ __tests__/screens/level-1.test.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Level1 } from '../../src/screens/level-1'
+import { render } from '@testing-library/react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+// fix react-navigation issues:
+// - https://github.com/react-navigation/react-navigation/issues/9727
+// - https://github.com/react-navigation/react-navigation/issues/9568
+jest.useFakeTimers()
+
+const Stack = createNativeStackNavigator()
+
+it('renders correctly', () => {
+  const component = (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Screen' component={Level1} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+
+  const { toJSON } = render(component)
+
+  expect(toJSON()).toMatchSnapshot()
+})
